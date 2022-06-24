ad350bf4e2b872fcb80121346657774b3680ecad
Vincent <jeremy.kerbidi@gmail.com>
2022-06-24 09:56:22 +0200

    [ADD] ew function with get fruit value onclick

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function addRecipe(e) {
     let recipeLink = document.getElementById('formula_link').value;
     let componentFormula = recipeFormula.match(/= (.*)*/);
     let calculationFormula = recipeFormula.match(/(.*)= */);
-    
+
     // Description
     let baliseRecipeFormula = document.createElement('a');
     let descriptionRecipe = document.createElement('p');
@@ -33,5 +33,39 @@ function addRecipe(e) {
     baliseRecipeFormula.className = "recipe_pin";
     document.getElementById('container').appendChild(baliseRecipeFormula);
 
+    e.preventDefault();
+}
+
+let count = 0;
+let btnAddFood = document.getElementById("button_add_food");
+// let i = 0;
+
+function addComponent(e) {
+// console.log(e);
+    // let firstCompFood = document.getElementById("first_component_food");
+    // let secondCompFood = document.getElementById("second_component_food");
+    // let thirdCompFood = document.getElementById("third_component_food");
+    
+    // i = 0;
+    let valuesComponent = document.getElementsByClassName("food_button");
+    i = 0;
+    i++;
+    console.log(e.target.innerHTML);
+    // const check = localStorage.setItem(e.target.value, "first_component");
+    // console.log(check);
+
+    count++;
+    // console.log(localStorage.getItem("first_component", e.target.value));
+    // if (e.target.value === localStorage.getItem("first_component")) {
+    //     console.log('TEST');
+    // firstCompFood.innerHTML = count;
+    // }
+    // else if () {
+    //     secondCompFood.innerHTML = count;
+    // } else {
+    //     thirdCompFood.innerHTML = count;
+    // }
+
     // e.preventDefault();
+    // console.log(count);
 }
